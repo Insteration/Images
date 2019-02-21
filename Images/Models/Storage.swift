355b57b2324b706328fddbd0a55608faeecee416
Artem Karmaz <inste@icloud.com>
2019-02-21 12:50:23 +0200

    five commit

--- Images/Models/Storage.swift
+++ Images/Models/Storage.swift
@@ -11,12 +11,12 @@ import Foundation
 struct Storage {
     
     var headlines = [
-        Headline(id: 1, title: "Raspberry", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim. Morbi convallis tincidunt quam eget bibendum. Suspendisse malesuada maximus ante, at molestie massa fringilla id.", image: "Raspberry"),
-        Headline(id: 2, title: "Apple", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim. Morbi convallis tincidunt quam eget bibendum. Suspendisse malesuada maximus ante, at molestie massa fringilla id.", image: "Apple"),
-        Headline(id: 3, title: "Banana", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non, finibus et diam. Morbi et felis a lacus pharetra blandit.", image: "Banana"),
-        Headline(id: 4, title: "Cantaloupe", text: "Aliquam egestas ultricies dapibus. Nam molestie nunc in ipsum vehicula accumsan quis sit amet quam. Sed vel feugiat eros.", image: "Cantaloupe"),
-        Headline(id: 5, title: "Strawberry", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim.", image: "Strawberry"),
-        Headline(id: 6, title: "Blueberry", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim.", image: "Blueberry"),
-        Headline(id: 7, title: "Cantaloupe", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim.", image: "Cantaloupe")
+        Headline(id: 1, title: "Raspberry", text: "The raspberry is the edible fruit of a multitude of plant species in the genus Rubus of the rose family, most of which are in the subgenus Idaeobatus; the name also applies to these plants themselves. Raspberries are perennial with woody stems.", image: "Raspberry"),
+        Headline(id: 2, title: "Apple", text: "An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek and European Christian traditions.", image: "Apple"),
+        Headline(id: 3, title: "Banana", text: "A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called plantains, distinguishing them from dessert bananas. ", image: "Banana"),
+        Headline(id: 4, title: "Cantaloupe", text: "Cantaloupe (muskmelon, mushmelon, rockmelon, sweet melon) or spanspek (South Africa) is a variety of the Cucumis melo species in the Cucurbitaceae family.", image: "Cantaloupe"),
+        Headline(id: 5, title: "Strawberry", text: "The garden strawberry (or simply strawberry; Fragaria × ananassa) is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries. It is cultivated worldwide for its fruit. The fruit is widely appreciated for its characteristic aroma, bright red color, juicy texture, and sweetness.", image: "Strawberry"),
+        Headline(id: 6, title: "Blueberry", text: "Blueberries are perennial flowering plants with blue– or purple–colored berries. They are classified in the section Cyanococcus within the genus Vaccinium.", image: "Blueberry"),
+        Headline(id: 7, title: "Cantaloupe", text: "Cantaloupe (muskmelon, mushmelon, rockmelon, sweet melon) or spanspek (South Africa) is a variety of the Cucumis melo species in the Cucurbitaceae family.", image: "Cantaloupe")
     ]
 }
